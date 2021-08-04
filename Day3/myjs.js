93ab220afcd17c372141c5b92772c55b2ed5fd61
Ashok <tashok@in.imshealth.com>
2021-08-04 12:28:16 +0530

    first commit

---
+++ Day3/myjs.js
@@ -0,0 +1,8 @@
+console.log('My script');
+
+document.getElementById("temp").innerHTML="Hi This is a Header";
+document.getElementById("temp").style.color='blue';
+function change()
+{
+    document.getElementById("temp").style.backgroundColor='pink';
+}
